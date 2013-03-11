46288f9089ebbf7dd64f8bf1465954b324480453
Alexander Weitzmann <alexander.weitzmann@stud.tu-darmstadt.de>
2013-03-11 15:13:24 +0100

    2PCommit.

--- src/OmazonEJBClient/ejbModule/de/tud/in/middleware/snapshot/EmployeeClientProxy.java
+++ src/OmazonEJBClient/ejbModule/de/tud/in/middleware/snapshot/EmployeeClientProxy.java
@@ -3,27 +3,25 @@ package de.tud.in.middleware.snapshot;
 import java.io.Serializable;
 import java.util.Random;
 
-
-public final class EmployeeClientProxy implements Serializable,  MobileClient{
+public final class EmployeeClientProxy implements Serializable, MobileClient {
 	private static final long serialVersionUID = -6014457740219416126L;
-	
+
 	public final Integer id = new Random().nextInt();
-	
-	private MobileManagementRemote mobileManagment;
-	
-	public EmployeeClientProxy(){
-		
+
+	private final MobileClient listener;
+
+	public EmployeeClientProxy(final MobileClient listener) {
+		this.listener = listener;
 	}
 
 	@Override
-	public boolean prepareToUpdateSnapshot(Snapshot snap) {
-		return false;
+	public boolean prepareToUpdateSnapshot(final Snapshot snap) {
+		return listener.prepareToUpdateSnapshot(snap);
 	}
 
 	@Override
-	public boolean updateToSnapshot(Integer id) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean updateToSnapshot(final Integer id) {
+		return listener.updateToSnapshot(id);
 	}
 
 }
